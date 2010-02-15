4310211c8c76a7ab1334e94b5caf98d48a475800
Rob Carlson <rob@solitude.home>
2010-02-14 23:07:15 -0500

    Split out md5 def into its own

--- preview.py
+++ preview.py
@@ -6,14 +6,11 @@ import itertools
 from datetime import datetime
 import hashlib
 
-path = '/home/rob/photos'
+path = '/local/photos'
 
 def exim_fetch(filename):
 	try:
 		f = open(filename, 'rb')
-		content = f.read()
-		md5hash = hashlib.md5(content).hexdigest()
-		f.seek(0)
 		tags = EXIF.process_file(f)
 		timelist = [ item.split(':') for item in str(tags['EXIF DateTimeOriginal']).split(' ') ] 
 		timelist = list(itertools.chain(*timelist))
@@ -22,9 +19,23 @@ def exim_fetch(filename):
 			when = datetime(timelist[0], timelist[1], timelist[2], timelist[3], timelist[4], timelist[5])
 		except ValueError:  # When 0000:00:00 00:00:00
 			return str(tags['EXIF DateTimeOriginal'])
-		return str(when), md5hash
+		return str(when)
 	except KeyError:
-		return '','',''
+		return ''
+	finally:
+		f.close()
+
+def md5_parse(filename):
+	try:
+		f = open(filename, 'rb')
+		content = f.read()
+		md5hash = hashlib.md5(content).hexdigest()
+		return md5hash
+	except:
+		return 'No hash'
+	finally:
+		f.close()
+	
 
 def import_images(dirname):
 	for f in os.listdir(dirname):
@@ -32,7 +43,7 @@ def import_images(dirname):
 		if os.path.isfile(fullpath):
 			#print os.path.splitext(f)[1]
 			if os.path.splitext(f)[1] in [".jpg",".JPG"]:
-				print fullpath, datetime.fromtimestamp(os.stat(fullpath).st_mtime), exim_fetch(fullpath)
+				print fullpath, datetime.fromtimestamp(os.stat(fullpath).st_mtime), exim_fetch(fullpath), md5_parse(fullpath)
 		if os.path.isdir(fullpath):
 			import_images(fullpath)
 
