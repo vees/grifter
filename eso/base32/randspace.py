a7eecbc5c103b80aca5826e208bf4d5c9132f305
Rob Carlson <rob@vees.net>
2014-08-18 20:41:58 -0400

    Convinced there's better logic in python to do this

--- eso/base32/randspace.py
+++ eso/base32/randspace.py
@@ -2,11 +2,12 @@ import random
 
 def randid():
 	"""Creates a random ID space with three modified
-base32 characters and one number in random order""" 
-	l=[chr(x) for x in range(97,97+26) if chr(x) not in list("ilou")]
+base32 characters and one number in random order for
+example vnt5, z2aa, tb9h or 2qxd""" 
+	r = lambda h: h[random.randint(0,len(h))-1]
+	l=[chr(x) for x in range(97,123) if chr(x) not in list("ilou")]
 	n=[chr(x) for x in range (48,58)]
-	e=[l[random.randint(0,len(l)-1)] for x in range(3)]
-	e+=[n[random.randint(0,len(n)-1)]]
+	e=[r(l) for x in range(3)] + [r(n)]
 	random.shuffle(e)
 	return e
 
